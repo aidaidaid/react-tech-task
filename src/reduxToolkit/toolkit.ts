82c00a9cb7c97cb19db9b6b67dd07f686f1b29b8
AidaShirinova <aidash@wizardsdev.com>
2023-04-22 22:29:12 +0400

    cleaning

--- src/reduxToolkit/toolkit.ts
+++ src/reduxToolkit/toolkit.ts
@@ -1,13 +1,7 @@
-// import { combineReducers } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import slice from "./toolkitSlice";
 
-// const rootReducer = combineReducers({
-//     toolkit: slice
-// })
 export const store = configureStore({
-    // reducer: slice,
-        reducer: {
-        toolkit: slice,
-    },
-})
+	reducer: {
+	toolkit: slice,
+}})
